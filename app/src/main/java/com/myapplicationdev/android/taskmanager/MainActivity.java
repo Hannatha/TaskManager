9f930541cb947f1d537212978e05b8abcd0c23b8
Ju Long <19013345@myrp.edu.sg>
2021-08-03 12:42:17 +0800

    Complete MainActivity.java and activity_main.xml

--- app/src/main/java/com/myapplicationdev/android/taskmanager/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/taskmanager/MainActivity.java
@@ -13,10 +13,41 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
+    Button btnAddTask;
+    ArrayList<Task> tasklist;
+    ArrayAdapter<Task> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listview = findViewById(R.id.listview);
+        btnAddTask = findViewById(R.id.btnAdd);
+        tasklist = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasklist);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tasklist.clear();
+
+        DBHelper dbHelper = new DBHelper(MainActivity.this);
+        tasklist.addAll(dbHelper.getTasks());
+        dbHelper.close();
+
+        adapter.notifyDataSetChanged();
     }
 }
